4598be1353e4610abfdebee29cda8ddaaa9daf70
baizhong <22268937112@qq.com>
2022-09-19 11:15:47 +0800

    init

---
+++ src/main/java/com/entire/hotspotmapapi/HotspotMapApiApplication.java
@@ -0,0 +1,13 @@
+package com.entire.hotspotmapapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotspotMapApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotspotMapApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/entire/hotspotmapapi/HotspotMapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entire.hotspotmapapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotspotMapApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
