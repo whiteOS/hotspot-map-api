379882ca1b0a27971971d2249b6b7c93f581237e
baizhong <22268937112@qq.com>
2022-09-24 10:38:32 +0800

    init

--- src/main/java/com/entire/hotspotmap/HotspotMapApplication.java
+++ src/main/java/com/entire/hotspotmap/HotspotMapApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HotspotMapApiApplication {
+public class HotspotMapApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HotspotMapApiApplication.class, args);
+        SpringApplication.run(HotspotMapApplication.class, args);
     }
 
 }
